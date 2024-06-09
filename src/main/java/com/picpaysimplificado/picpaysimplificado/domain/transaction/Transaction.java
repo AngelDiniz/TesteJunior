9b78087e2ad823d1e227d4f9d311881ab454ed4e
Angel Pereira Diniz <angelpdiniz@gmail.com>
2024-06-09 17:00:19 -0300

    feat: create user, repositories.

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,38 @@
+package com.picpaysimplificado.picpaysimplificado.domain.transaction;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.catalina.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transaction")
+@Table(name = "transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name ="receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+
+}
